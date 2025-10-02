68a1caa1e70559d6b3f705969519dc1687e3e11d
birukdjn <155588225+birukdjn@users.noreply.github.com>
2025-10-03 00:27:44 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Projects from "./Pages/projects";
 import Experience from "./Pages/experience";
 import Blogs from "./Pages/blogs";
 import Contact from "./Pages/contact";
-import TerminalIntro from "./components/terminalIntro";
+import TerminalIntro from "./components/terminallntro";
 
 
 export default function Home() {
