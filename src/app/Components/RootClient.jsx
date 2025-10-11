b4a86539e6a169564fb42ef4d0afd1bdeaddcf70
Biruk Dejene <birukedjn@gmail.com>
2025-10-11 18:31:15 +0300

    session storage updated

--- src/app/Components/RootClient.jsx
+++ src/app/Components/RootClient.jsx
@@ -1,16 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TerminalIntro from "./terminalIntro";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
 export default function RootClient({ children }) {
   const [showContent, setShowContent] = useState(false);
+  const [hasPlayed, setHasPlayed] = useState(false);
+
+  // Check if intro already played this session
+  useEffect(() => {
+    const played = sessionStorage.getItem("introPlayed");
+    if (played) {
+      setShowContent(true);
+      setHasPlayed(true);
+    }
+  }, []);
+
+  // When intro finishes, save it in sessionStorage
+  const handleFinish = () => {
+    sessionStorage.setItem("introPlayed", "true");
+    setShowContent(true);
+    setHasPlayed(true);
+  };
 
   return (
     <>
-      {!showContent && <TerminalIntro onFinish={() => setShowContent(true)} />}
+      {!hasPlayed && !showContent && (
+        <TerminalIntro onFinish={handleFinish} />
+      )}
 
       {showContent && (
         <>
